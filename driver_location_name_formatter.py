73e4cab0f8337c6de6d3b54f4f56c2c1931164c3
Fabliha Fairooz <fablihafairooz@gmail.com>
2021-04-05 23:12:19 +0600

    Update driver_location_name_formatter.py

--- driver_location_name_formatter.py
+++ driver_location_name_formatter.py
@@ -7,23 +7,7 @@
 import pandas as pd
 from location_name_formatter import LocationNameFormatter
 
-
-# In[2]:
-
-
 name_formatter = LocationNameFormatter()
-
-
-# In[3]:
-
-
 before_format = "Chittagong"
 formatted_string = name_formatter.formatter(before_format)
 print(formatted_string)
-
-
-# In[ ]:
-
-
-
-
